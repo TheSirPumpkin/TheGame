98f17ea7bbcc9a9fc60c31c3f534bfe5ffab3e25
TheSirPumpkin <djakovd2013@yahoo.com>
2016-11-20 14:43:02 +0300

    Add files via upload

---
+++ Assets/RPG/Health.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	public AudioSource deathAudio;
+	public AudioSource ScaryAudio;
+	public float HitPoints = 100f;
+	public bool Boss;
+	public bool FullHealth;
+	public bool Network; 
+	//public bool SaveTest;
+	public bool Dead=false;
+
+	public void ReciveDamage (float amount){
+						
+		if (HitPoints == 100f) {FullHealth=true;}
+		if (HitPoints < 100f) {FullHealth=false;}
+
+			HitPoints -= amount;
+
+		if (HitPoints > 0) {
+			ScaryAudio.Play ();
+		}
+				if (HitPoints <= 0) {
+			deathAudio.Play ();
+						Die ();
+				}
+		}
+		 void Die()
+	{if (Boss) {Application.LoadLevel (3);}
+		if (Network) {PhotonNetwork.Destroy (gameObject);};
+		Destroy(gameObject);
+		Dead = true;}
+	
+}
---
+++ Assets/RPG/HealthPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPlayer : MonoBehaviour {
+	public float health;
+	public GUIText HealthBar;
+	public bool lvl1;
+	public bool lvl2;
+	public bool lvl3;
+	public bool lvl4;
+	// Use this for initialization
+	void Start () {  
+	}
+	
+	// Update is called once per frame
+	void Update () {HealthBar.text = "Health"+":" + health;
+		EnemyAI.PlayerIsAlive = true;
+		if (health<=0)
+		{
+			if(lvl2){Application.LoadLevel (7);}
+			else if(lvl1){Application.LoadLevel (4);}
+			else if(lvl3){Application.LoadLevel (12);}
+			else if(lvl4){Application.LoadLevel (14);}
+			Destroy(gameObject);
+			EnemyAI.PlayerIsAlive = false;
+		}
+	}
+}
